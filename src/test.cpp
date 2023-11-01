a6220086119cf0360c6f42c0272af96c10449dad
HH-LG <2474674768@qq.com>
2023-11-01 16:04:39 +0800

    add debug

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstring>
 #include "test.h"
+#include "mini.h"
 
 extern int regularPrase();
 extern int yyparse();
 extern FILE *yyin;
 
 using namespace std;
+bool test_regex(int id)
+{
+    int match_cnt = 0;
+    string filename = "./test/eg/example" + to_string(id) + ".txt";
+    ifstream fin(filename);
+    char input[256];
+    while (match_cnt < 100)
+    {
+        fin.getline(input, 256, '\n'); //getline(char *,int,char) 表示该行字符达到256个或遇到换行就结束
+        int len = strlen(input);
+        int cur_state = 0;
+        for (int i = 0; i < len ; i++)
+        {
+            cur_state = dfa_simplified.graph[cur_state][input[i]];
+        }
+        if (dfa_simplified.finish_state.find(cur_state) != dfa_simplified.finish_state.end())
+        {
+            match_cnt ++;
+        }
+        else
+        {
+            cout << "cur_state: " << cur_state << endl;
+            cout << "test " << id << ", example " << match_cnt << " failed" << endl;
+            fin.close();
+            return false;
+        }
+    }
+    fin.close();
+    return true;
+}
 
 void test_dfa_simplified(char* testfile)
 {
@@ -26,14 +59,14 @@ void test_dfa_simplified(char* testfile)
     do
     {
         yyparse();
-
-        cnt ++;
+        if (!test_regex(cnt))
+        {
+            cout << "test " << cnt << " failed" << endl;
+            exit(EXIT_FAILURE);
+        }
         cout << "cnt:" << cnt << endl;
+        cnt ++;
     } while (!feof(yyin));
     
 }
 
-bool test_regex(int id)
-{
-
-}
